829fc37286043d74985fcac99500c5981d646a1d
arun-curefit <arun.k@curefit.com>
2020-04-06 17:33:21 +0530

    import fix.

--- src/daos/DelayWindowReadonlyDaoMongoImpl.ts
+++ src/daos/DelayWindowReadonlyDaoMongoImpl.ts
@@ -1,10 +1,10 @@
 import { injectable, inject } from "inversify"
 import { FLASH_MODELS_TYPES } from "../ioc/FlashModelsTypes"
 import { BASE_TYPES, ILogger } from "@curefit/base"
-import { DelayWindowSchema } from "../models/DelayWindow/DelayWindowSchema"
+import { DelayWindowSchema } from "../models/delayWindow/DelayWindowSchema"
 import { MongoReadonlyDao } from "@curefit/mongo-utils"
 import { DelayWindow } from "@curefit/flash-common"
-import { DelayWindowModel } from "../models/DelayWindow/DelayWindowModel"
+import { DelayWindowModel } from "../models/delayWindow/DelayWindowModel"
 import { IDelayWindowReadonlyDao } from "./IDelayWindowDao"
 
 @injectable()
