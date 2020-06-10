65b52b2e60ad0928212b18b22e3bdf4a58d86bfb
arun-curefit <41287935+arun-curefit@users.noreply.github.com>
2020-06-10 16:44:51 +0530

    Added new index in lot. removed lotIds from passbook. (#18)
    * Added new index in lot. removed lotIds from passbook.
    
    * Added index on ruleCard and startDate.
    
    * Updated flash-common.

--- src/models/ppo/Schemas.ts
+++ src/models/ppo/Schemas.ts
@@ -141,10 +141,6 @@ const PilotPassbookEntrySchemaObject = {
     required: true,
     index: true,
   },
-  lotIds: {
-    type: [String],
-    required: true,
-  },
   surgePricing: {
     type: [TempSchema2Object],
     required: false,
@@ -201,7 +197,8 @@ export class PilotPassbookEntrySchema extends MultiMongooseSchema<PilotPassbookE
     return [
       { pilotId: 1, startDate: 1, endDate: 1, centerId: 1 },
       { centerId: 1, startDate: 1, endDate: 1 },
-      { pilotId: 1, centerId: 1 }
+      { pilotId: 1, centerId: 1 },
+      { ruleCardId: 1, startDate: 1 }
     ]
   }
 }
